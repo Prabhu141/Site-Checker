12531961d8b0e764821e355d8179ea627c047126
Prabhu141 <102575437+Prabhu141@users.noreply.github.com>
2022-12-08 10:29:15 +0530

    Add files via upload

--- mysite/core/urls.py
+++ mysite/core/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('csvform/', views.csv_upload, name='csv_upload'),
     path('csv/', views.csvs, name='csvs'),
+    path('csv/<int:id>/',views.delete_document,name='delete_document'),
     path('sitemapurl/', views.sitemap_upload, name='sitemap_upload'),
     path('sitemapxml/', views.sitemap, name='sitemap'),
 ]
